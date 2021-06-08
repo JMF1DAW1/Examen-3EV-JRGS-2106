ca4aa8960e225573aedbde8eaef4739c1254d68e
Melissa Garcia <garcia20143@iesmarenostrum.com>
2021-06-08 20:33:29 +0200

    JMF2021 He modificado el código y he añadido pruebas unitarias.

--- ClassLibrary1/ClassLibrary1/Examen3EV.cs
+++ ClassLibrary1/ClassLibrary1/Examen3EV.cs
@@ -1,70 +1,127 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
+// Examen realizado por Javier Marco Francisco 1º DAW Y 2021
 namespace Examen3EV_NS
 {
-    public class estNot  // esta clase nos calcula las estadísticas de un conjunto de notas 
+    // esta clase nos calcula las estadísticas de un conjunto de notas 
+    public class JMF2021EstadisticasNotas 
     {
-        public int sus;  // Suspensos
-        public int apr;  // Aprobados
-        public int not;  // Notables
-        public int sbr;  // Sobresalientes
+        private int suspensos;
+        private int aprobados;
+        private int notables;
+        private int sobresalientes;
 
-        public double media; // Nota media
+        public double media;
+
+        public int Suspensos 
+        { 
+            get => suspensos; 
+            set => suspensos = value; 
+        }
+        public int Aprobados 
+        { 
+            get => aprobados; 
+            set => aprobados = value; 
+        }
+        public int Notables 
+        {
+            get => notables; 
+            set => notables = value; 
+        }
+        public int Sobresalientes 
+        { 
+            get => sobresalientes;
+            set => sobresalientes = value; 
+        }
 
         // Constructor vacío
-        public estNot()
+        public JMF2021EstadisticasNotas()
         {
-            sus = apr = not = sbr = 0;  // inicializamos las variables
+            Suspensos = 0;
+            Aprobados = 0;
+            Notables = 0;
+            Sobresalientes = 0; 
             media = 0.0;
         }
 
         // Constructor a partir de un array, calcula las estadísticas al crear el objeto
-        public estNot(List<int> listnot)
+        public JMF2021EstadisticasNotas(List<int> listaNotas)
         {
+            Suspensos = 0;
+            Aprobados = 0;
+            notables = 0;
+            Sobresalientes = 0;
             media = 0.0;
 
-            for (int i = 0; i < listnot.Count; i++)
-            {
-                if (listnot[i] < 5) sus++;              // Por debajo de 5 suspenso
-                else if (listnot[i] > 5 && listnot[i] < 7) apr++;// Nota para aprobar: 5
-                else if (listnot[i] > 7 && listnot[i] < 9) not++;// Nota para notable: 7
-                else if (listnot[i] > 9) sbr++;         // Nota para sobresaliente: 9
-
-                media = media + listnot[i];
-            }
-
-            media = media / listnot.Count;
+            CalcularEstadisticas(listaNotas);
         }
 
 
-        // Para un conjunto de listnot, calculamos las estadísticas
+        // Para un conjunto de listaNotas, calculamos las estadísticas
         // calcula la media y el número de suspensos/aprobados/notables/sobresalientes
-        //
-        // El método devuelve -1 si ha habido algún problema, la media en caso contrario	
-        public double calcEst(List<int> listnot)
+
+        // Agregamos estas constantes para hacer una prueba más especifica. 
+        public const string NotaPorDebajoDe0 = "Nota que está por debajo de 0";
+        public const string NotaPorEncimade10 = "Nota que está por encima de 10";
+
+        /// <summary>
+        /// Método que calcula las estadísticas
+        /// </summary>
+        /// <param name="listaNotas"> Listado que le pasará el usuario con las lista de notas. </param>
+        /// <returns> Devuelve el calculo de la media</returns>
+        /// <remarks> En esta función se devolverán diferentes errores en caso de fallar. </remarks>
+        public double CalcularEstadisticas(List<int> listaNotas)
         {                                 
             media = 0.0;
 
-            // TODO: hay que modificar el tratamiento de errores para generar excepciones
-            //
-            if (listnot.Count <= 0)  // Si la lista no contiene elementos, devolvemos un error
-                return -1;
-
-            for (int i=0;i<10;i++)
-                if (listnot[i] < 0 || listnot[i] > 10)      // comprobamos que las not están entre 0 y 10 (mínimo y máximo), si no, error
-                return -1;
-
-            for (int i = 0; i < listnot.Count; i++)
-            {
-                if (listnot[i] < 5) sus++;              // Por debajo de 5 suspenso
-                else if (listnot[i] >= 5 && listnot[i] < 7) apr++;// Nota para aprobar: 5
-                else if (listnot[i] >= 7 && listnot[i] < 9) not++;// Nota para notable: 7
-                else if (listnot[i] > 9) sbr++;         // Nota para sobresaliente: 9
-
-                media = media + listnot[i];
+            // Si la lista no contiene elementos, devolvemos un error
+            if (listaNotas.Count <= 0)
+            {        
+                throw new Exception("La lista no contiene elementos.");
             }
 
-            media = media / listnot.Count;
+            for (int i = 0; i < listaNotas.Count; i++)
+            {
+                // Si es menor que 0, da error. 
+                if (listaNotas[i] < 0)
+                {
+                    throw new ArgumentOutOfRangeException("nota", listaNotas[i], NotaPorDebajoDe0);
+                }
+                // Si es mayor que 0, da error. 
+                if (listaNotas[i] > 10)
+                {
+                    throw new ArgumentOutOfRangeException("nota", listaNotas[i], NotaPorEncimade10);
+                }
+            }
+          
+            for (int i = 0; i < listaNotas.Count; i++)
+            {
+                // Por debajo de 5 suspensos
+                if (listaNotas[i] < 5)
+                {
+                    Suspensos++;
+                }
+                // Nota para aprobar: 5          
+                else if (listaNotas[i] >= 5 && listaNotas[i] < 7)
+                {
+                    Aprobados++;
+                }
+                // Nota para notables: 7  
+                else if (listaNotas[i] >= 7 && listaNotas[i] < 9)
+                {
+                    Notables++;
+                }
+                // Nota para sobresalientes: 9
+                else if (listaNotas[i] >= 9)
+                {
+                    Sobresalientes++;
+                }                     
+
+                media = media + listaNotas[i];
+            }
+
+            media = media / listaNotas.Count;
 
             return media;
         }
--- ClassLibrary1/UnitTestProject1/UnitTest1.cs
+++ ClassLibrary1/UnitTestProject1/UnitTest1.cs
@@ -1,14 +1,208 @@
 ﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Examen3EV_NS;
+using System;
 
+// Examen realizado por Javier Marco Francisco 1º DAW Y 2021
 namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        [ExpectedException(typeof(Exception))]
+        public void ListaVacia()
+        {
+            List<int> notas = new List<int>();
+            JMF2021EstadisticasNotas prueba = new JMF2021EstadisticasNotas();
+
+            // Acción a probar
+            prueba.CalcularEstadisticas(notas);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NotaMenorque0()
+        {
+            List<int> notas = new List<int>();
+
+
+            notas.Add(-1);
+            JMF2021EstadisticasNotas prueba = new JMF2021EstadisticasNotas();
+            // Acción a probar
+            prueba.CalcularEstadisticas(notas);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NotaMayorQue10()
+        {
+            List<int> notas = new List<int>();
+
+            notas.Add(11);
+
+            JMF2021EstadisticasNotas prueba = new JMF2021EstadisticasNotas();
+
+            // Acción a probar
+            prueba.CalcularEstadisticas(notas);
+        }
+
+        [TestMethod]
+        public void ContadorSuspensos()
+        {
+            List<int> notas = new List<int>();
+            JMF2021EstadisticasNotas prueba = new JMF2021EstadisticasNotas();
+            notas.Add(0);
+            notas.Add(5);
+            notas.Add(9);
+            notas.Add(3);
+            notas.Add(7);
+            notas.Add(4);
+            notas.Add(8);
+
+            int ContadorSuspensos = 3;
+
+            // Acción a probar
+            prueba.CalcularEstadisticas(notas);
+
+            // actual
+            int actual = prueba.Suspensos;
+
+            Assert.AreEqual(actual, ContadorSuspensos, 0.001, "No da el resultado esperado");
+        }
+
+        [TestMethod]
+        public void ContadorAprobados()
+        {
+            List<int> notas = new List<int>();
+            JMF2021EstadisticasNotas prueba = new JMF2021EstadisticasNotas();
+            notas.Add(5);
+            notas.Add(6);
+            notas.Add(7);
+
+            int ContadorAprobados = 2;
+
+            // Acción a probar
+            prueba.CalcularEstadisticas(notas);
+
+            // Actual
+            int actual = prueba.Aprobados;
+
+            Assert.AreEqual(actual, ContadorAprobados, 0.001, "No da el resultado esperado");
+        }
+        [TestMethod]
+        public void ContadorAprobadosPorDefecto()
+        {
+            List<int> notas = new List<int>();
+            JMF2021EstadisticasNotas prueba = new JMF2021EstadisticasNotas();
+            notas.Add(0);
+            notas.Add(5);
+            notas.Add(9);
+            notas.Add(3);
+            notas.Add(7);
+            notas.Add(4);
+            notas.Add(8);
+
+            int ContadorAprobados = 1;
+
+            // Acción a probar
+            prueba.CalcularEstadisticas(notas);
+
+            // Actual
+            int actual = prueba.Aprobados;
+
+            Assert.AreEqual(actual, ContadorAprobados, 0.001, "No da el resultado esperado");
+        }
+
+        [TestMethod]
+        public void ContadorNotables()
+        {
+            List<int> notas = new List<int>();
+            JMF2021EstadisticasNotas prueba = new JMF2021EstadisticasNotas();
+            notas.Add(7);
+            notas.Add(8);
+            notas.Add(9);
+
+            int ContadorNotables = 2;
+
+            // Acción a probar
+            prueba.CalcularEstadisticas(notas);
+
+            // Actual
+            int actual = prueba.Notables;
+
+            Assert.AreEqual(actual, ContadorNotables, 0.001, "No da el resultado esperado");
+        }
+
+        [TestMethod]
+        public void ContadorSobresalientes()
+        {
+            List<int> notas = new List<int>();
+            JMF2021EstadisticasNotas prueba = new JMF2021EstadisticasNotas();
+            notas.Add(8);
+            notas.Add(9);
+            notas.Add(10);
+
+            int ContadorSobresalientes = 2;
+
+            // Acción a probar
+            prueba.CalcularEstadisticas(notas);
+
+            // Actual
+            int actual = prueba.Sobresalientes;
+
+            Assert.AreEqual(actual, ContadorSobresalientes, 0.001, "No da el resultado esperado.");
+        }
+
+
+        [TestMethod]
+        public void MediaEsperada()
+        {
+            List<int> notas = new List<int>();
+            JMF2021EstadisticasNotas prueba = new JMF2021EstadisticasNotas();
+            notas.Add(0);
+            notas.Add(5);
+            notas.Add(9);
+            notas.Add(3);
+            notas.Add(7);
+            notas.Add(4);
+            notas.Add(8);
+
+            double mediaEsperada = 5.143;
+
+            // Acción a probar
+            prueba.CalcularEstadisticas(notas);
+
+            // Actual
+            double actual = prueba.media;
+
+            Assert.AreEqual(mediaEsperada, actual, 0.001, "No da la misma media");
+        }
+
+        [TestMethod]
+        public void NotaMayorQue10Mejorado()
+        {
+            try
+            {
+                List<int> notas = new List<int>();
+
+                notas.Add(11);
+
+                JMF2021EstadisticasNotas prueba = new JMF2021EstadisticasNotas();
+
+                // Acción a probar
+                prueba.CalcularEstadisticas(notas);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                StringAssert.Contains(e.Message, JMF2021EstadisticasNotas.NotaPorEncimade10, "Error");
+                return;
+            }
+            Assert.Fail("Error");        
+        }
+
+        [TestMethod]
+        public void TestConstructorMedia()
         {
             List<int> notas = new List<int>();
 
@@ -21,10 +215,14 @@ namespace UnitTestProject1
             notas.Add(8);
 
             double mediaEsperada = 5.143;
-            int susE = 3;
-            int aprE = 1;
-            int notE = 2;
-            int sbrE = 1;
+
+            // Acción a probar
+            JMF2021EstadisticasNotas prueba = new JMF2021EstadisticasNotas(notas);
+
+            // Actual
+            double actual = prueba.media;
+
+            Assert.AreEqual(mediaEsperada, actual, 0.001, "No da la misma media");
         }
     }
 }
